e604a1906016b6438de2d865c3301a114a4a16f0
CaramelmanEX <chisanupong2407@gmail.com>
2026-04-24 13:54:41 +0700

    del test text

--- src/components/atomic-crm/layout/Header.tsx
+++ src/components/atomic-crm/layout/Header.tsx
@@ -32,7 +32,6 @@ const Header = () => {
       <nav className="grow">
         <header className="bg-secondary">
           <div className="px-4">
-            <div>test</div>
             <div className="flex justify-between items-center flex-1">
               <Link
                 to="/"
